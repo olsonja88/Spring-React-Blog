1304741d320514f984a4b15e931e56043e754b26
jaolson88 <olsonja88@gmail.com>
2023-07-05 12:48:01 -0500

    Feat: Create directory for custom scss files, create directory for custom css.  Compile custom bootstrap from custom.scss and import it in the head of app.js for custom bootstrap styles throughout the entire app.

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/custom_bootstrap.css';
 
 import Posts from './components/Posts';
 import Navbar from './components/BlogNav';
 
 function App() {
-  return (
-    <div className="main-container">
-      <Navbar />
-      <Posts />
-    </div>
-  );
+    return (
+        <div className="main-container">
+            <Navbar />
+            <Posts />
+        </div>
+    );
 }
 
 export default App;
