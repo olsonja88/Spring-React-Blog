d8a7f1603a8e64541cba36de4fc73b6e85bc9495
jaolson88 <olsonja88@gmail.com>
2023-07-07 09:56:58 -0500

    Fix: Add bootstrap, react-bootstrap, and fix imports in app.js, both apps build and run successfully via maven.

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/css/custom.css";
+import Posts from './components/Posts';
+import Navbar from './components/BlogNav';
 
 function App() {
   return (
