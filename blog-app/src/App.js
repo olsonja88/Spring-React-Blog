97be6c7916b284a16a97eef52e3efa0a8f2f2d75
jaolson88 <olsonja88@gmail.com>
2023-07-07 09:49:34 -0500

    Fix: Recreate spring-boot app with backend dependencies JPA and H2.  Recreate react apps blog-app and admin-app.  Move old code for blog-app over.

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,15 +1,13 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "./assets/css/custom.css";
-import Posts from './components/Posts';
-import Navbar from './components/BlogNav';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <div className="main-container">
+  return (
+    <div className="main-container">
             <Navbar />
             <Posts />
         </div>
-    );
+  );
 }
 
 export default App;
--- src/test/java/com/example/springbootblog/SpringBootBlogApplicationTests.java
+++ src/test/java/com/example/springbootblog/SpringBootBlogApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.blogproject;
+package com.example.springbootblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlogProjectApplicationTests {
+class SpringBootBlogApplicationTests {
 
 	@Test
 	void contextLoads() {
