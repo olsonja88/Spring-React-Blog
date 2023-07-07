aa4d4446dd72964bb96f34853ff08266f59575b8
jaolson88 <olsonja88@gmail.com>
2023-07-07 12:24:31 -0500

    Feat: Add dashboard component and icons.

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -2,11 +2,14 @@ import { Admin, Resource } from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
+import { Dashboard } from "./Dashboard";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name ="users" list={UserList} recordRepresentation="name" />    
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+    <Resource name="posts" list={PostList} icon={PostIcon} edit={PostEdit} create={PostCreate} />
+    <Resource name ="users" list={UserList} icon={UserIcon} recordRepresentation="name" />    
   </Admin>
 );
 
