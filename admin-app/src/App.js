573e2b0e64dec6c81c2d337c397926f5ffb60aaf
jaolson88 <olsonja88@gmail.com>
2023-07-07 12:42:28 -0500

    Feat: Add authentication and login page.

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -3,11 +3,12 @@ import { dataProvider } from "./components/dataProvider";
 import { UserList } from "./components/users";
 import { PostList, PostEdit, PostCreate } from "./components/posts";
 import { Dashboard } from "./components/Dashboard";
+import { authProvider } from "./components/authProvider";
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 
 const App = () => (
-  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
+  <Admin dataProvider={dataProvider} authProvider={authProvider} dashboard={Dashboard}>
     <Resource name="posts" list={PostList} icon={PostIcon} edit={PostEdit} create={PostCreate} />
     <Resource name ="users" list={UserList} icon={UserIcon} recordRepresentation="name" />    
   </Admin>
