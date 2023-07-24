196232a62d96cdcd91ed704215703d698883455a
jaolson88 <olsonja88@gmail.com>
2023-07-24 15:45:56 -0500

    Feat: Add bootstrap import to index.js, make App.js disply a list of the current available categories.

--- creator-app/src/App.js
+++ creator-app/src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('api/categories')
+      .then(response => response.json)
+      .then(data => {
+        setCategories(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-intro">
+          <h2>Category List</h2>
+          {categories.map(category =>
+            <div key={category.id}>
+              {category.name}
+            </div>
+            )}
+        </div>
       </header>
     </div>
   );
