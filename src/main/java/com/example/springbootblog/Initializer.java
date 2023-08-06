c605bbd28280cf9d11807a1fe8c22ad9edbcd00a
jaolson88 <olsonja88@gmail.com>
2023-08-06 14:52:49 -0500

    Fix: Create one post for each category in the Initializer.

--- src/main/java/com/example/springbootblog/Initializer.java
+++ src/main/java/com/example/springbootblog/Initializer.java
@@ -22,18 +22,37 @@ class Initializer implements CommandLineRunner {
 
     @Override
     public void run(String... strings) {
-        Stream.of("Web Development", "Mobile Development", "Game Development",
-        "Embedded Systems").forEach(name -> 
+        Stream.of("Web Development", "Mobile Development", "Game Development").forEach(name -> 
         repository.save(new Category(name))
         );
 
         Category catOne = repository.findByName("Web Development");
-        Post postOne = Post.builder().title("Best Comp Sci Resources")
-            .content("Here are the best resources to learn Comp Sci!")
+        Post postOne = Post.builder().title("First Time Making a Web App")
+            .content("Building a web application is hard, but that doesn't mean you shouldn't try!  " + 
+            "In my experience it definitely helps to read a lot of different tutorials, lots of stackoverflow, and be patient.")
             .date(Instant.now())
             .build();
         catOne.setPosts(Collections.singleton(postOne));
         repository.save(catOne);
+
+        Category catTwo = repository.findByName("Mobile Development");
+        Post postTwo = Post.builder().title("Best Projects for Beginning Mobile Development")
+            .content("Looking for ideas to begin learning mobile development?  Here are some great projects to get you started.  " +
+            "Simple Task Manager, Weather App, Quiz App, Expense Tracker, Recipe Finder, Movie Recommendation, Habit Tracker.")
+            .date(Instant.now())
+            .build();
+        catTwo.setPosts(Collections.singleton(postTwo));
+        repository.save(catTwo);
+
+        Category catThree = repository.findByName("Game Development");
+        Post postThree = Post.builder().title("Game Testing")
+            .content("Game testing is arguably the most important part of game development.  " +
+            "After each new feature is created, it's important to make sure that you get the feel right.  " + 
+            "Another benefit of game testing is finding bugs.  It's important to find as many as you can before release!")
+            .date(Instant.now())
+            .build();
+        catThree.setPosts(Collections.singleton(postThree));
+        repository.save(catThree);
 
         repository.findAll().forEach(System.out::println);
     }
