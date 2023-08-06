39d0a25ba945e473440194b64e2bf271b2495134
jaolson88 <olsonja88@gmail.com>
2023-08-06 14:30:22 -0500

    Fix: Remove logo.svg and webvitals from blog-app to clean up the file structure.

--- ui/blog-app/src/App.js
+++ ui/blog-app/src/App.js
@@ -1,12 +1,7 @@
-// import Posts from './components/Posts';
-// import Navbar from './components/BlogNav';
-
-import logo from './logo.svg';
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/css/custom.css";
 import Home from './components/Home';
-import BlogNav from './components/BlogNav';
 import CategoryList from './components/CategoryList';
 import CategoryEdit from './components/CategoryEdit';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
