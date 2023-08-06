408a11f71d197c71c3ef9beda7c988fe888c1f7c
jaolson88 <olsonja88@gmail.com>
2023-08-06 14:27:50 -0500

    Feat: Create PostController and PostRepository to begin managing posts within the app.

---
+++ src/main/java/com/example/springbootblog/model/PostRepository.java
@@ -0,0 +1,7 @@
+package com.example.springbootblog.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Post findByName(String name);
+}
--- src/main/java/com/example/springbootblog/web/PostController.java
+++ src/main/java/com/example/springbootblog/web/PostController.java
@@ -1,5 +1,21 @@
 package com.example.springbootblog.web;
 
+import com.example.springbootblog.model.Post;
+import com.example.springbootblog.model.PostRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
+
 public class PostController {
     
 }
