279b41236ff31676321d1b2473bce4ba9c3d1b62
jaolson88 <olsonja88@gmail.com>
2023-08-07 09:06:32 -0500

    Fix: Finish all methods for PostController, add useful comments to CategoryController and PostController to explain what's going on in the code.

--- src/main/java/com/example/springbootblog/web/CategoryController.java
+++ src/main/java/com/example/springbootblog/web/CategoryController.java
@@ -19,51 +19,84 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
+
+    // For log statements
     private final Logger log = LoggerFactory.getLogger(CategoryController.class);
-    
+
+    // Collection of categories
     private CategoryRepository categoryRepository;
 
 
+    // Constructor
     public CategoryController(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
 
 
+    // Get all categories from the repository
     @GetMapping("/categories")
     Collection<Category> categories() {
+
+        // Return all categories in the repository
         return categoryRepository.findAll();
     }
 
 
+    // Get specific category from the repository
     @GetMapping("/category/{id}")
     ResponseEntity<?> getCategory(@PathVariable Long id) {
+
+        // Store the category we want into an "optional" variable in case it doesn't exist
         Optional<Category> category = categoryRepository.findById(id);
+
+        // Return a response holding the data from the specific category, return "not found" if it doesn't exist
         return category.map(response -> ResponseEntity.ok().body(response))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
 
+    // Add a new category to the repository
     @PostMapping("/category")
     ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) throws URISyntaxException {
+        
+        // Log our request in the terminal 
         log.info("Request to create category: {}", category);
+
+        // Save the new category into the repository, store the return value in "result"
         Category result = categoryRepository.save(category);
+
+        // Return a response with the URI of the new category
         return ResponseEntity.created(new URI("/api/category" + result.getId()))
                 .body(result);
     }
 
 
+    // Update or edit an already existing category in the repository
     @PutMapping("/category/{id}")
     ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category) {
+        
+        // Log our request in the terminal
         log.info("Request to update category: {}", category);
+
+        // Save the updated category into the repositorym store the return value in "result"
         Category result = categoryRepository.save(category);
+
+        // Return a successful response with the data from the updated category 
         return ResponseEntity.ok().body(result);
     }
 
     
+    // Remove a category from the repository
     @DeleteMapping("/category/{id}")
     public ResponseEntity<?> deleteCategory(@PathVariable Long id) {
+        
+        // Log our request in the terminal
         log.info("Request to delete category: {}", id);
+
+        // Delete the category from the repository
         categoryRepository.deleteById(id);
+
+        // Return a successful response with no data
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/springbootblog/web/PostController.java
+++ src/main/java/com/example/springbootblog/web/PostController.java
@@ -16,6 +16,87 @@ import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/api")
 public class PostController {
     
+    // For log statements
+    private final Logger log = LoggerFactory.getLogger(PostController.class);
+
+    // Collection of posts
+    private PostRepository postRepository;
+
+
+    // Constructor
+    public PostController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+
+    // Get all posts from the repository
+    @GetMapping("/posts")
+    Collection<Post> posts() {
+
+        // Return all posts in the repository
+        return postRepository.findAll();
+    }
+
+
+    // Get specific post from the repository
+    @GetMapping("/posts/{id}")
+    ResponseEntity<?> getPost(@PathVariable Long id) {
+
+        // Make an "optional" variable to the hold the post we want in case it doesn't exist
+        Optional<Post> post = postRepository.findById(id);
+
+        // Return a response with the data from the found post, if it doesn't exist then return "not found"
+        return post.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
+    // Add a new post to the repository
+    @PostMapping("/post")
+    ResponseEntity<Post> createPost(@Valid @RequestBody Post post) throws URISyntaxException {
+
+        // Log our request in the terminal
+        log.info("Request to create post: {}", post);
+
+        // Save our new post in the repository, store the return value in "result"
+        Post result = postRepository.save(post);
+
+        // Return a "created", 201 response with the URI of the new post
+        return ResponseEntity.created(new URI("/api/post" + result.getId()))
+                .body(result);
+    }
+
+
+    // Update or edit an exists post in the repository
+    @PutMapping("/post/{id}")
+    ResponseEntity<Post> updatePost(@Valid @RequestBody Post post) {
+
+        // Log our request in the terminal
+        log.info("Request to update post: {}", post);
+
+        // Save the new post into the repository, save the return value into result
+        Post result = postRepository.save(post);
+
+        // Return a successful status with the new post data
+        return ResponseEntity.ok().body(result);
+    }
+
+
+    // Remove a category from the repository
+    @DeleteMapping("/post/{id}")
+    public ResponseEntity<?> deletePost(@PathVariable Long id) {
+
+        // Log our request in the terminal
+        log.info("Request to delete post: {}", id);
+
+        // Remove the post from the repository
+        postRepository.deleteById(id);
+
+        // Indicate successful result without returning any data
+        return ResponseEntity.ok().build();
+    }
 }
